015db64956a223d9655d907fcc9f1a9275bf0c4c
prakharmosesOK <mosesprakhar@gmail.com>
2024-08-17 16:56:49 +0530

    Chat

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -20,12 +20,12 @@ export default function ChatBot() {
 
 
             {chatOpen && (<div style={{boxShadow: '0 0 #0000, 0 0 #0000, 0 1px 2px 0 rgb(0 0 0 / 0.05)'}}
-                className="fixed bottom-[calc(4rem+1.5rem)] right-0 mr-4 bg-white p-6 rounded-lg border border-[#e5e7eb] w-[30vw] h-[70vh]">
+                className="fixed z-[100] bottom-[calc(4rem+1.5rem)] right-0 mr-4 bg-neutral-50 dark:bg-neutral-950 p-6 rounded-lg border border-[#e5e7eb] dark:border-[#282829] w-[30vw] h-[70vh]">
 
                 {/* <!-- Heading --> */}
                 <div className="flex flex-col space-y-1.5 pb-6">
                     <h2 className="font-semibold text-lg tracking-tight text-slate-800 dark:text-slate-500">Chatbot</h2>
-                    <p className="text-sm text-[#6b7280] leading-3">Angirasoft</p>
+                    <p className="text-sm text-[#6b7280] dark:text-[#b4bac6] leading-3">Angirasoft</p>
                 </div>
 
 
@@ -43,7 +43,7 @@ export default function ChatBot() {
                             </path>
                         </svg></div>
                     </span>
-                        <p className="leading-relaxed"><span className="block font-bold text-gray-700">AI </span>Hi, how can I help you today?
+                        <p className="leading-relaxed text-gray-600 dark:text-gray-400"><span className="block font-bold text-gray-700 dark:text-gray-300">AI </span>Hi, how can I help you today?
                         </p>
                     </div>
 
@@ -57,7 +57,7 @@ export default function ChatBot() {
                             </path>
                         </svg></div>
                     </span>
-                        <p className="leading-relaxed"><span className="block font-bold text-gray-700">You </span>fewafef</p>
+                        <p className="leading-relaxed text-gray-600 dark:text-gray-400"><span className="block font-bold text-gray-700 dark:text-gray-300">You </span>fewafef</p>
                     </div>
                     {/* <!-- Ai Chat Message  --> */}
                     <div className="flex gap-3 my-4 text-gray-600 text-sm flex-1"><span
@@ -69,7 +69,7 @@ export default function ChatBot() {
                             </path>
                         </svg></div>
                     </span>
-                        <p className="leading-relaxed"><span className="block font-bold text-gray-700">AI </span>Sorry, I couldn't find any
+                        <p className="leading-relaxed text-gray-600 dark:text-gray-400"><span className="block font-bold text-gray-700 dark:text-gray-300">AI </span>Sorry, I couldn't find any
                             information in the documentation about that. Expect answer to be less accurateI could not find the answer to
                             this in the verified sources.</p>
                     </div>
@@ -81,7 +81,7 @@ export default function ChatBot() {
                             className="flex h-10 w-full rounded-md border border-[#e5e7eb] px-3 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
                             placeholder="Type your message" value="" />
                             <button
-                                className="inline-flex items-center justify-center rounded-md text-sm font-medium text-[#f9fafb] disabled:pointer-events-none disabled:opacity-50 bg-black hover:bg-[#111827E6] h-10 px-4 py-2">
+                                className="inline-flex items-center justify-center rounded-md text-sm font-medium text-[#f9fafb] dark:text-[#131313] disabled:pointer-events-none disabled:opacity-50 bg-black dark:bg-white hover:bg-[#111827E6] dark:hover:bg-[#cecfd3e6] h-10 px-4 py-2">
                                 Send</button>
                     </form>
                 </div>
