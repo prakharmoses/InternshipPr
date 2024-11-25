1adcee90a14cd00db1ec5d353d36e5044d66d814
prakharmoses <me21b054@iittp.ac.in>
2024-11-25 12:33:19 +0530

    Deploy settings

--- src/config/helmetConfig.js
+++ src/config/helmetConfig.js
@@ -4,8 +4,8 @@ const helmetConfig = () => helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", 'data:', 'validator.swagger.io'],
+        styleSrc: ["'self'", "'unsafe-inline'", "https:"],
+        imgSrc: ["'self'", 'data:', 'validator.swagger.io', "https:"],
         connectSrc: ["'self'"],
         fontSrc: ["'self'"],
         objectSrc: ["'none'"],
