ec6825492436b11c20f1302e568baa6783516770
prakharmoses <me21b054@iittp.ac.in>
2025-07-25 18:15:29 +0530

    CORS issue resolved

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://internship-pr-5rzy.vercel.app/',
+    '*',
     'https://internship-pr.vercel.app/',
 ]
 
